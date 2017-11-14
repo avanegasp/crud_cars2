db8bbfe146154fb20afe529a1532da781cb5af76
avanegasp <arco527@gmail.com>
2017-11-14 11:49:18 -0500

    Crud done

--- app/controllers/app.rb
+++ app/controllers/app.rb
@@ -15,7 +15,40 @@ get '/cars/new' do
   erb :"cars/new"
 end
 
+
 #create
 post '/cars' do
-  Car.create(brand:[params:brand], model:params[:model], img_url: params[:img_url])
+  Car.create(brand: params[:brand], model:params[:model], img_url: params[:img_url])
+end
+
+#show
+get '/cars/:id' do
+  @car = Car.find(params[:id])
+  erb :"cars/show"
+end
+
+#edit
+get '/cars/:id/edit' do
+  @car = Car.find(params[:id])
+  erb :"cars/edit"
+end
+
+#update
+patch '/cars/:id' do
+  @car =Car.find(params[:id])
+  @car.brand = params[:brand]
+  @car.model = params[:model]
+  @car.img_url = params[:img_url]
+  if @car.save
+    redirect "/cars"
+  else
+    erb :edit
+  end
+end
+
+#delete
+delete '/cars/:id' do
+  car = Car.find(params[:id])
+  car.destroy
+  redirect "/cars"
 end
